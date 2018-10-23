b60488a3d535692f8e4277bada771aca2f085d44
gokel166 <devgwo18@gmail.com>
2018-10-23 05:31:04 -0500

    add new teams component

--- src/app/teams.ts
+++ src/app/teams.ts
@@ -1,2 +1,15 @@
 export class Teams {
+    id: Number;
+    teamName: String;
+    city: String;
+    teamState: String;
+    division: String;
+
+    constructor(id: Number, teamName: String, city: String, teamState: String, division: String) {
+        this.id = id;
+        this.teamName = teamName;
+        this.city = city;
+        this.teamState = teamState;
+        this.division = division;
+    }
 }
