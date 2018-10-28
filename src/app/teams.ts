8bc9e3e53140dc1125d4444d7aec9f35fc37588d
gokel166 <devgwo18@gmail.com>
2018-10-28 03:33:07 -0500

    Add form and button submission

--- src/app/teams.ts
+++ src/app/teams.ts
@@ -1,12 +1,10 @@
 export class Teams {
-    public id: Number;
     public teamName: String;
     public city: String;
     public teamState: String;
     public division: String;
 
-    constructor(id: Number, teamName: String, city: String, teamState: String, division: String) {
-        this.id = id;
+    constructor(teamName: String, city: String, teamState: String, division: String) {
         this.teamName = teamName;
         this.city = city;
         this.teamState = teamState;
--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -11,12 +11,12 @@ import { EventEmitter } from 'events';
 export class TeamsComponent implements OnInit {
 
   teams = [
-    new Teams(1, 'Pistons', 'Detroit', 'Michigan', 'Central Division'),
-    new Teams(2, 'Celtics', 'Boston', 'Massachusetts', 'Atlantic Division'),
-    new Teams(3, 'Clippers', 'Los Angeles', 'California', 'Pacific Division'),
-    new Teams(4, 'Pelicans', 'New Orleans', 'Lousiana', 'Southwest Division'),
-    new Teams(5, 'Trailblazers', 'Portland', 'Oregon', 'Northwest Division'),
-    new Teams(6, 'Heat', 'Miami', 'Florida', 'Southeast Division')
+    new Teams('Pistons', 'Detroit', 'Michigan', 'Central Division'),
+    new Teams('Celtics', 'Boston', 'Massachusetts', 'Atlantic Division'),
+    new Teams('Clippers', 'Los Angeles', 'California', 'Pacific Division'),
+    new Teams('Pelicans', 'New Orleans', 'Lousiana', 'Southwest Division'),
+    new Teams('Trailblazers', 'Portland', 'Oregon', 'Northwest Division'),
+    new Teams('Heat', 'Miami', 'Florida', 'Southeast Division')
   ]
 
   myTeam = this.teams[0];
